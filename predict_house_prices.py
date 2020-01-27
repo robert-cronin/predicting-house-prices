8c67ee04fa5d01dc214fe19562b6ed02fde12eb1
Robbie Cronin <robert.cronin@uqconnect.edu.au>
2020-01-27 13:44:01 +0800

    add model creation using statsmodels.api

--- predict_house_prices.py
+++ predict_house_prices.py
@@ -1,3 +1,4 @@
+import statsmodels.api as stats
 import pandas as pd
 import numpy as np
 import operator
@@ -5,6 +6,9 @@ import operator
 class PredictHousePrices():
     def __init__(self):
         self.train_data = pd.read_csv("data/train.csv")
+        self.learn_data = self.train_data[0:int(self.train_data.shape[0]*0.8)]
+        self.validate_data = self.train_data[int(self.train_data.shape[0]*0.8):-1]
+
         self.test_data = pd.read_csv("data/test.csv")
 
     # def data_prep(self):
@@ -21,9 +25,24 @@ class PredictHousePrices():
     def calculate_correlation(self, col):
         return self.train_data[col].corr(self.train_data["SalePrice"])
 
+    def build_linreg_model(self, col):
+        model = stats.OLS(self.learn_data["SalePrice"], self.learn_data[col]).fit()
+        return model
+
 
 
 if __name__ == "__main__":
     sol = PredictHousePrices()
     print("Top 5 correlated numeric columns are: ")
-    print(sol.top_correlated_columns())
+    corr_cols = sol.top_correlated_columns()
+    for col, corr in corr_cols.items():
+        print(col+": ", corr)
+    for col in corr_cols:
+        model = sol.build_linreg_model(col)
+
+        predictions = model.predict(sol.validate_data[col])
+
+        # Print out the statistics
+        model.summary()
+        print("Predictions for "+col+": ")
+        print(predictions.head())
